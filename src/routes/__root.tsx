f0558aff1c340cff32d083bd28923a691e73ea5d
Angel S. Moreno <angelxmoreno@gmail.com>
2025-02-08 14:30:33 -0500

    hiding TanStack RouterDevTools via hack

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,21 @@
 import {createRootRoute, Outlet} from '@tanstack/react-router'
-import {TanStackRouterDevtools} from '@tanstack/router-devtools'
 import AppHeader from "@components/layout/AppHeader";
 import {Container} from "react-bootstrap";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+
+const TanStackRouterDevtools =
+    import.meta.env.MODE === 'production'
+        ? () => null // Render nothing in production
+        : React.lazy(() =>
+            // Lazy load in development
+            import('@tanstack/router-devtools').then((res) => ({
+                default: res.TanStackRouterDevtools,
+                // For Embedded Mode
+                // default: res.TanStackRouterDevtoolsPanel
+            })),
+        )
 
 export const Route = createRootRoute({
     component: () => (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,6 +10,8 @@ function Index() {
       <Row>
           <Col>
               <h1>Welcome to Serial Killer Wiki</h1>
+              <h6>{import.meta.env.MODE}</h6>
+              <h6>{process.env.NODE_ENV}</h6>
               <p>
                   This is a placeholder homepage. We are currently building out the content,
                   so please check back soon for more updates!
